1ed7b2de712f0309ef3a338a6b41441dd8094132
pijiulaoshi <pijiulaoshi@gmail.com>
2020-05-14 22:04:31 +0200

    Create __init__.py

---
+++ custom_components/philips_somneo/__init__.py
@@ -0,0 +1,56 @@
+"""Philips Somneo All V1"""
+import asyncio
+import logging
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.discovery import load_platform
+from homeassistant.const import TEMP_CELSIUS
+
+from .const import *
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup(hass, config):
+    """Setup the Somneo component."""
+    try:
+        conf = config[DOMAIN]
+        entity_id = DOMAIN + ".status"
+        sensor_id = DOMAIN + ".sensors"
+        name = conf.get(CONF_NAME, DEFAULT_NAME)
+        host = conf.get(CONF_HOST, DEFAULT_HOST)
+        port_int = conf.get(CONF_PORT, DEFAULT_PORT)
+        port = str(port_int)
+        sens_url = 'https://' + host + ':' + port + '/di/v1/products/1/wusrd'
+        light_url = 'https://' + host + ':' + port + '/di/v1/products/1/wulgt'
+        sens = []
+        for xsensor in conf.get(CONF_SENS):
+            sens.append(xsensor)
+        ### DATA_SETS ###
+        host_data = {ATTR_C_NAME: name, ATTR_C_HOST: host, ATTR_C_PORT: port, ATTR_C_SENS: sens}
+        sens_data = {ATTR_S_TEMP: None, ATTR_S_HUM: None, ATTR_S_LIGHT: None, ATTR_S_NOISE: None}
+        light_data = {ATTR_L_ONOFF: None, ATTR_L_LTLVL: None, ATTR_L_CTYPE: None, ATTR_L_NGTLT: None}
+        data = {"component": host_data, "sensors": sens_data, "light": light_data}
+        hass.data[DATA_PSC] = host_data
+        hass.data[DOMAIN] = data
+
+        load_platform(hass, 'light', DOMAIN, {}, config)
+        hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+
+        
+        hass.states.set(entity_id, "Active", data)
+        #### NOTHING BELOW THIS LINE ####
+        # If Success:
+        _LOGGER.info("Somneo_V1 has been set up!")
+        return True
+    except Exception as ex:
+        _LOGGER.error('Error while initializing Somneo, exception: {}'.format(str(ex)))
+        hass.components.persistent_notification.create(
+            f'Error: {str(ex)}<br />Fix issue and restart',
+            title=NOTIFICATION_TITLE,
+            notification_id=NOTIFICATION_ID)
+        # If Fail:
+        return False
+
+
+
